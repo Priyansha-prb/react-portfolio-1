bb798f54878cafd200167b8b0d3bf22cf442aad3
Ram Nayudu <Ram.Nayudu@quisitive.com>
2021-08-21 01:34:19 +0530

    Updates unused

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 const Contact = ({ data }) => {
-   const [url, setUrl] = useState('mailto:prinsmediainc@gmail.com?subject=subject&body=body');
    const [name, setName] = useState('');
    const [subject, setSubject] = useState('');
    const [email, setEmail] = useState('');
