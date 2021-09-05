e83e1c4f0cd2adfefdd7ec806f522d3033dbb930
Ram Nayudu <Ram.Nayudu@quisitive.com>
2021-09-05 23:34:21 +0530

    Error Fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import Portfolio from './Components/Portfolio';
-import TechStack from './Components/TechStack';
 
 class App extends Component {
 
